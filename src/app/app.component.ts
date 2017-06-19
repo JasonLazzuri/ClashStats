fb9a5722e03a722750fe9c391d2d56b776f91efe
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-06-19 13:39:04 -0700

    implement twitter api widget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
-}
+
+  ngOnInit () {
+              !function(d,s,id){
+                  var js: any,
+                      fjs=d.getElementsByTagName(s)[0],
+                      p='https';
+                  if(!d.getElementById(id)){
+                      js=d.createElement(s);
+                      js.id=id;
+                      js.src=p+"://platform.twitter.com/widgets.js";
+                      fjs.parentNode.insertBefore(js,fjs);
+                  }
+              }
+              (document,"script","twitter-wjs");
+      }
+  }
